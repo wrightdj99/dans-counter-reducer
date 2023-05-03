4cf0f05fc8775b32d9bf0056c34806435260837d
wrightdj99 <wrightdj99@gmail.com>
2023-05-03 11:53:14 -0500

    Finished With Hooks

--- src/prod-pages/Counter.jsx
+++ src/prod-pages/Counter.jsx
@@ -10,12 +10,13 @@ function Counter() {
     const decrementCount = () => {
         changeCount(count - 1);
     }
+
     return(
         <div>
             <h1>Hello, I'm the Counter!</h1>
             <h2>Count: {count}</h2>
-            <button onClick={incrementCount}>Add</button>
-            <button onClick={decrementCount}>Subtract</button>
+            <button onClick={incrementCount} className="buttonClass">Add</button>
+            <button onClick={decrementCount} className="buttonClass">Subtract</button>
         </div>
     );
 }
